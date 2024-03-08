fd329dc04aa6580ec70923e1d4b23a07b5a30a81
DanielR <113525395+Shimotakii@users.noreply.github.com>
2024-03-08 08:39:17 +0100

    Add files via upload

--- Validations/validation.js
+++ Validations/validation.js
@@ -30,3 +30,44 @@ passwordInp.addEventListener("focus", function (ev) {
 
 // Wenn der Benutzer ein Passwort will, und die Taste loslässt (keyup) dann soll überprüft ob das Passwort 8 Zeichen lang ist
 // wenn nicht dann sollte eine Fehlermeldung kommen bzgl 8 Zeichenlängen und wie viel Zeichen man momentan hat
+
+passwordInp.addEventListener("keyup", function (ev) {
+    if (passwordInp.validity.tooShort) {
+        passwordInp.setCustomValidity(`Das Passwort muss mindestens 8 Zeichen lang sein. Gerade hat es ${passwordInp.value.length} Zeichen.`);
+        passwordInp.reportValidity();
+    } else {
+        passwordInp.setCustomValidity("");
+    }
+})
+
+const zipCode = document.querySelector("#zipCode");
+const country = document.querySelector("#country");
+
+
+function checkPlz() {
+    let constraints= {
+        ch: ['^(X-)?\\d{4}$', "Schweizerische PLZ müssen aus genau 4 Zeichen bestehen: e.g. CH-1950 or 1950"],
+        fr: ['^(F-)?\\d{5}$', "Französische PLZ müssen aus genau 5 Zeichen bestehen: e.g. F-75012 or 75012"],
+        de: ['^(D-)?\\d{5}$', "Deutsche PLZ müssen aus genau 5 Zeichen bestehen: e.g. D-12345 or 12345"]
+    }
+
+    const countryValue = country.value;
+    const zipCodeValue = zipCode.value;
+
+    let constraint = new RegExp(constraints[countryValue][0], "");
+
+    if(constraint.test(zipCodeValue)) {
+        zipCode.setCustomValidity("");
+    } else {
+        zipCode.setCustomValidity(constraints[countryValue][1]);
+        zipCode.reportValidity();
+    }
+}
+
+zipCode.addEventListener("input", checkPlz);
+country.addEventListener("input", checkPlz);
+
+document.querySelector("form").addEventListener("submit", function(ev) {
+    // Auf die Form angewendet verhindert event.preventDefault() Das neuladen der seite bei senden der Form
+    ev.preventDefault();
+})
